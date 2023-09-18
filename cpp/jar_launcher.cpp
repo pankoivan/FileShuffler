27bf14390fc4c33c9a0e32628b2813909e170e03
pankoivan <panivan98@yandex.ru>
2023-09-18 22:52:48 +0300

    Добавил cpp-файл, экзешник которого используется для запуска jar программы, и архив, внутри которого находятся jar, exe, настроечный файл и файл readme

---
+++ cpp/jar_launcher.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	system("java -jar file_shuffler.jar");
+	cout << "\n\n\nJava program \"file_shuffler.jar\" has finished its work\n\n\n";
+	system("pause");
+	return 0;
+}
